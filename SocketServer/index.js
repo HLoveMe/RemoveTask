c458e2d78845710fd7ff466982f41d2c0b744eeb
Clear Zhu | Bindo <clear.zhu@bindo.com>
2020-08-03 20:01:25 +0800

    init3

--- SocketServer/index.js
+++ SocketServer/index.js
@@ -1,13 +1,17 @@
 const WebSocket = require('ws');
+const path = require("path");
+const fs = require('fs');
 const wss = new WebSocket.Server({
   host: "127.0.0.1",
   port: 8080
 });
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
-    let aaaa = require("./message.json");
-    ws.send(JSON.stringify(aaaa));
+    // console.log('received: %s', message);
+    const data = fs.readFileSync(path.join(__dirname,"message.json"));
+    console.log("11111",data)
+    ws.send(data);
+    
   });
 
   ws.send('something');
