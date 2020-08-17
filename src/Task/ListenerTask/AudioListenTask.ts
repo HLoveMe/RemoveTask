f6b003a76ac4bf9152756e490703645c4cec36f1
Clear Zhu | Bindo <clear.zhu@bindo.com>
2020-08-17 21:36:09 +0800

    audio

--- src/Task/ListenerTask/AudioListenTask.ts
+++ src/Task/ListenerTask/AudioListenTask.ts
@@ -1,19 +1,26 @@
 
 /***
+ * 
+ * 待测试
  * https://sourceforge.net/projects/sox/  下载
  * 
  * alias play="/Users/swl/Downloads/sox-14.4.2/play"
  * alias soxi="/Users/swl/Downloads/sox-14.4.2/soxi"
  * alias sox="/Users/swl/Downloads/sox-14.4.2/sox"
  */
-// https://github.com/MexXxo/node-microphone
+
+ /**
+  * 
+  * https://github.com/noffle/electron-speech
+  */
+
 
 import { TaskStatus, ListenTask, App } from "../Base/TaskBase";
 import { ScreenshotMessage, ScreenshotCMD, ImageInfo, ScreenshotData, Message } from "../../WebSocket/SocketMessage";
 import PathConfig from "../../Util/PathRUL";
 import { join } from "path";
 import { existsSync, unlinkSync, readFileSync } from "fs";
-const Microphone = require('node-microphone');
+// const Microphone = require('node-microphone');
 const maxCount: number = 50;
 
 /**{id: 1000,key: 1000,date: 10000,name: "AudioListenTask",data: {}} */
